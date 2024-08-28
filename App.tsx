8ae42e42c92864ca721305d9258e61449f64d5cb
raphael <rafavfx1@gmail.com>
2024-08-27 23:22:00 -0300

    criando os estilos

--- App.tsx
+++ App.tsx
@@ -6,9 +6,13 @@ import { Constants } from 'expo-constants';
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle='dark-content'></StatusBar>
       <Text style={styles.textHeader}>Faça sua pergunta!</Text>
       
-      <ImageBackground style={styles.image}>
+      <ImageBackground 
+          source={require('./assets/wizarddd.png')} 
+          style={styles.image}
+        >
         <Text style={styles.textResult}></Text>
         
         <TouchableOpacity style={styles.button}>
@@ -21,5 +25,34 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    // paddingTop: Constants.statusBarHeight
+  },  
+  textHeader: {
+    fontSize: 30,
+    textAlign: 'center', 
+    padding: 15,
+    backgroundColor: '#000',
+    color: 'rgb(255, 200, 30)'
+  },
+  image: {
+    flex: 1, 
+  },
+  textResult: {
+
+  }, 
+  button: {
+    backgroundColor: 'rgba(255, 200, 30, 0.5)',
+    width: 300, 
+    height: 100,
+    borderRadius: 60, 
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textButton: {
+    fontSize: 40, 
+    fontWeight: 'bold'
+  }
+
 })
